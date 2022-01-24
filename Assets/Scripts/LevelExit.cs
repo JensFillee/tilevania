e410759bddc10feed1ec7544829ce0defdc621ea
JensFillee <r0800028@student.thomasmore.be>
2022-01-24 17:17:54 +0100

    added end-level portals

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float loadLevelDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(loadNextLevel());
+    }
+
+    // Coroutines: used to create a delay
+    IEnumerator loadNextLevel()
+    {
+        // do immediately when method is called (BEFORE WaitForSecondsRealtime(loadNextLevelDelay);)
+
+
+        // wait for WaitForSecondsRealtime(loadNextLevelDelay) to finish (async, in meantime: other things can happen (in other methods))
+        yield return new WaitForSecondsRealtime(loadLevelDelay);
+
+
+        // do AFTER WaitForSecondsRealtime(loadNextLevelDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // If this is the last level
+        if (nextSceneIndex == SceneManager.sceneCount)
+        {
+            // next level = level 1
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
